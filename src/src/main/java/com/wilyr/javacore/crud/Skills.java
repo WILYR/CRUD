0766c56dc83c77139c35af1637af2299e53a646a
Nikita\WILYR <wilyrjvdv@gmail.com>
2020-09-10 15:31:17 +0300

    git init

---
+++ src/src/main/java/com/wilyr/javacore/crud/Skills.java
@@ -0,0 +1,28 @@
+package src.main.java.com.wilyr.javacore.crud;
+
+public class Skills {
+    private String name;
+    private int id;
+
+    public Skills(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
