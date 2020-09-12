d640f8b4b6b57dfb9db2815a4fcb69794d2d550b
Nikita\WILYR <wilyrjvdv@gmail.com>
2020-09-12 10:21:16 +0300

    rework

--- src/src/main/java/com/wilyr/javacore/crud/Main.java
+++ src/src/main/java/com/wilyr/javacore/crud/Main.java
@@ -1,84 +1,14 @@
 package src.main.java.com.wilyr.javacore.crud;
 
-import java.io.*;
-import java.util.ArrayList;
-
 public class Main {
 
     public static void main(String[] args) {
-        Skills skill1 = new Skills("Maven", 1);
-        Skills skill2 = new Skills("Core", 2);
-        Skills skill3 = new Skills("SQL", 3);
-
-        add(skill1);
-        add(skill2);
-        add(skill3);
-        getAll();
-        delete("Core");
-        get("Maven");
-
-        update(skill1, "Gradle");
-    }
-
-    public static void add(Skills skill) {
-        try (FileWriter writer = new FileWriter("skills.txt", true)) {
-            writer.write(skill.getId() + ", " + skill.getName() + "\n");
-        } catch (IOException e) {
-            e.getMessage();
-        }
-    }
-
-    public static void delete(String remove) {
-        File oldFile = new File("skills.txt");
-
-        try {
-            FileReader fr = new FileReader(oldFile);
-            BufferedReader br = new BufferedReader(fr);
-            ArrayList<String> list = new ArrayList<>();
-            String line = br.readLine();
-            while (line != null) {
-                if (!line.contains(remove))
-                    list.add(line);
-                line = br.readLine();
-            }
-            fr.close();
-            br.close();
-            FileWriter fw = new FileWriter("skills.txt");
-            for (String i : list) {
-                fw.write(i + "\n");
-            }
-            fw.close();
-        } catch (IOException e) {
-            e.getMessage();
-        }
-    }
-
-    public static void getAll() {
-        try (FileReader reader = new FileReader("skills.txt")) {
-            int i;
-            while ((i = reader.read()) != -1) {
-                System.out.print((char) i);
-            }
-        } catch (IOException e) {
-            e.getMessage();
-        }
-    }
-
-    public static void get(String get) {
-        try (BufferedReader reader = new BufferedReader(new FileReader("skills.txt"))) {
-            String line = reader.readLine();
-            while (line != null) {
-                if (line.contains(get))
-                    System.out.println(line);
-                line = reader.readLine();
-            }
-        } catch (IOException e) {
-            e.getMessage();
-        }
-    }
-    public static void update(Skills skill, String newName) {
-        delete(skill.getName());
-        Skills newSkill = new Skills(newName, skill.getId());
-        add(newSkill);
+        Skills skill = new Skills();
+        skill.add(1, "Maven");
+        skill.add(2, "Core");
+        skill.delete(1);
+        skill.add(1, "Maven");
+        skill.get(1);
+        skill.update(1, "Gradle");
     }
 }
--- src/src/main/java/com/wilyr/javacore/crud/Skills.java
+++ src/src/main/java/com/wilyr/javacore/crud/Skills.java
@@ -1,28 +1,64 @@
 package src.main.java.com.wilyr.javacore.crud;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class Skills {
     private String name;
     private int id;
 
-    public Skills(String name, int id) {
-        this.name = name;
-        this.id = id;
+    public HashMap<Integer, String> getAll() {
+        HashMap<Integer, String> hashMap = new HashMap<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader("skills.txt"))) {
+            String line = reader.readLine();
+            while (line != null) {
+                String[] lineSplit = line.split(",");
+                hashMap.put(Integer.parseInt(lineSplit[0]), lineSplit[1]);
+                line = reader.readLine();
+            }
+        } catch (IOException e) {
+            e.getMessage();
+        }
+        return hashMap;
     }
 
-
-    public String getName() {
-        return name;
+    public void add(int id, String name) {
+        HashMap<Integer, String> hashMap = getAll();
+        if(!hashMap.containsKey(id)) {
+            try (FileWriter writer = new FileWriter("skills.txt", true)) {
+                writer.write(id + "," + name + "\n");
+            } catch (IOException e) {
+                e.getMessage();
+            }
+        }
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void delete (Integer id){
+        HashMap<Integer, String> hashMap = getAll();
+        hashMap.remove(id);
+        try (FileWriter writer = new FileWriter("skills.txt")) {
+            Set<Map.Entry<Integer, String>> set= hashMap.entrySet();
+            for(Map.Entry<Integer, String> i: set) {
+                writer.write(i.getKey() + "," + i.getValue() + "\n");
+            }
+        } catch (IOException e) {
+            e.getMessage();
+        }
     }
 
-    public int getId() {
-        return id;
+    public String get(int id){
+        HashMap<Integer, String> hashMap = getAll();
+        return hashMap.get(id);
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void update(int id, String name){
+        delete(id);
+        add(id, name);
     }
+
 }
